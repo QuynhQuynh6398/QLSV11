0baa5b1e87ce845835bae95ec77299f4df723e5e
QuynhQuynh6398 <quynhnguyen1906002@gmail.com>
2025-02-08 16:25:14 +0700

    Create sinhvien.java

---
+++ sinhvien.java
@@ -0,0 +1,84 @@
+package model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author HP
+ */
+public class sinhvien {
+    private String masv;
+    private String tensv;
+    private Date ngaysinh;
+    private boolean gioitinh;
+    private String diachi;
+    private String hinhanh;
+
+    public sinhvien() {
+    }
+
+    public sinhvien(String masv, String tensv) {
+        this.masv = masv;
+        this.tensv = tensv;
+    }
+
+    
+    
+    public sinhvien(String masv, String tensv, Date ngaysinh, boolean gioitinh, String diachi, String hinhanh) {
+        this.masv = masv;
+        this.tensv = tensv;
+        this.ngaysinh = ngaysinh;
+        this.gioitinh = gioitinh;
+        this.diachi = diachi;
+        this.hinhanh = hinhanh;
+    }
+
+    public String getMasv() {
+        return masv;
+    }
+
+    public void setMasv(String masv) {
+        this.masv = masv;
+    }
+
+    public String getTensv() {
+        return tensv;
+    }
+
+    public void setTensv(String tensv) {
+        this.tensv = tensv;
+    }
+
+    public Date getNgaysinh() {
+        return ngaysinh;
+    }
+
+    public void setNgaysinh(Date ngaysinh) {
+        this.ngaysinh = ngaysinh;
+    }
+
+    public boolean isGioitinh() {
+        return gioitinh;
+    }
+
+    public void setGioitinh(boolean gioitinh) {
+        this.gioitinh = gioitinh;
+    }
+
+    public String getDiachi() {
+        return diachi;
+    }
+
+    public void setDiachi(String diachi) {
+        this.diachi = diachi;
+    }
+
+    public String getHinhanh() {
+        return hinhanh;
+    }
+
+    public void setHinhanh(String hinhanh) {
+        this.hinhanh = hinhanh;
+    }
+    
+}
