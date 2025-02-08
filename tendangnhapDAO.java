af48b33e832af884d3788622e1eec49516abbc7f
QuynhQuynh6398 <quynhnguyen1906002@gmail.com>
2025-02-08 16:27:49 +0700

    Create tendangnhapDAO.java

---
+++ tendangnhapDAO.java
@@ -0,0 +1,29 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+/**
+ *
+ * @author HP
+ */
+public class tendangnhapDAO {
+    List<tendangnhap> ls = new ArrayList<>();
+
+    public tendangnhapDAO() {
+        ls.add(new tendangnhap("admin", "12345", true));
+        ls.add(new tendangnhap("Son", "123456", true));
+        ls.add(new tendangnhap("Dang", "1234567", true));
+        ls.add(new tendangnhap("Hoa", "1234578", true));
+      
+    }
+    
+    public boolean checkDangNhap(String username, String password){
+        for(tendangnhap t: ls){
+            if(t.getUsername().equals(username)&& t.getPassword().equals(password)){
+                return true;
+            }
+        }
+        return false;
+    }
+    
+}
